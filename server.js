08cf59ad191c71896c7cbb73d0b7faf9a71e9785
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-08-07 23:13:35 -0500

    additions

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var path = require('path');
 var exphbs  = require('express-handlebars');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 var methodOverride = require('method-override'); // for puts and deletes in express
 var PORT    = process.env.PORT || 3000;
 
 var models = require('./models');
 
 // override POST to have DELETE and PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -25,7 +25,7 @@ var router = express.Router();
 
 // sample route with a route the way we're used to seeing it
 app.get('/', function(req, res) {
-    res.render('home');  
+    res.render('home');
 });
 
 app.get('/users/:user_id/listings/', function(req, res){
@@ -63,7 +63,7 @@ app.get('/users/:id?/', function(req, res){
             res.render('user/index', { users: users});
         });
     }
-    
+
 });
 
 app.get('/listings', function(req, res){
